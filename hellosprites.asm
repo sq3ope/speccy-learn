f0278bfd0d38b8b3b49e211d63414965f9e2075f
Pawel Firlej <pawel.firlej@payu.com>
2025-11-03 21:28:17 +0100

    Check vertical limits

--- hellosprites.asm
+++ hellosprites.asm
@@ -27,7 +27,10 @@ scanKeysLoopNoPush:
                 call    ScanKeys                ; read keys pressed
 testQKey:
                 bit     0, d                    ; test Q key
-                jr      z, testAKey             ; if not pressed, test A key
+                jr      z, testAKey             ; if not pressed, test another key
+                ld      a, SCREEN_TOP           ; check upper boundary
+                call    CheckTop
+                jr      z, testAKey             ; if reached, skip moving up
                 ld      a, l                    ; move frog up
                 sub     %100000                 ; move up one row
                 ld      l, a
@@ -37,7 +40,10 @@ testQKey:
                 ld      h, a
 testAKey:
                 bit     1, d                    ; test A key
-                jr      z, testOKey             ; if not pressed, test O key
+                jr      z, testOKey             ; if not pressed, test another key
+                ld      a, SCREEN_BOTTOM        ; check lower boundary
+                call    CheckBottom
+                jr      z, testOKey             ; if reached, skip moving down
                 ld      a, l                    ; move frog down
                 add     a, %100000              ; move down one row
                 ld      l, a
@@ -47,7 +53,7 @@ testAKey:
                 ld      h, a
 testOKey:
                 bit     2, d                    ; test O key
-                jr      z, testPKey             ; if not pressed, test P key
+                jr      z, testPKey             ; if not pressed, test another key
                 dec     hl                      ; move frog left (decrease hl by 1)
 testPKey:
                 bit     3, d                    ; test P key
@@ -81,11 +87,23 @@ exit:
                 ret
 
 ;;;;;;; data area
+
+; the frog sprite occupies 2x2 characters (16x16 pixels)
+; the drwspr routine draws starting from bottom-left character so this character should be pointed by hl
+; therefore the upper limit (SCREEN_TOP) should be set to second character row (row number 1)
+; and the lower limit (SCREEN_BOTTOM) should be set to the last character row
+
+;;;;;;;;;;;;;;;;;;;;;;;; TT$LLL$SSS
+SCREEN_TOP      EQU     %00$001$000
+SCREEN_BOTTOM   EQU     %10$111$000
+
+
                 include "system_routines.asm"
                 include "sprite_routines.asm"
                 include "keyboard.asm"
                 include "sprite_data.asm"
                 include "chessboard.asm"
                 include "time.asm"
+                include "limits.asm"
 
                 end     $8000
---
+++ limits.asm
@@ -0,0 +1,56 @@
+;--------------------------------------------------------------------
+; Evaluates whether the lower limit has been reached.
+; Input:  A  -> Upper limit (TTLLLLSSS).
+;         HL -> Current position (010TTSSS LLLCCCCCCC).
+; Output: Z  =  Reached.
+;         NZ =  Not reached.
+;
+; Alters the value of the AF and BC registers.
+;--------------------------------------------------------------------
+CheckBottom:
+call            checkVerticalLimit              ; Compare current position with limit
+                                                ; If Z or NC has reached the ceiling, Z is set, otherwise NZ is set.
+ret             c
+checkBottom_bottom:
+xor             a                               ; Active Z
+ret
+
+;--------------------------------------------------------------------
+; Evaluates whether the upper limit has been reached.
+; Input:  A  -> Upper margin (TTLLLLSSS).
+;         HL -> Current position (010TTSSS LLLCCCCCCC).
+; Output: Z  =  Reached.
+;         NZ =  Not reached.
+;
+; Alters the value of the AF and BC registers.
+;--------------------------------------------------------------------
+CheckTop:
+call            checkVerticalLimit              ; Compare current position with limit
+ret                                             ; checkVerticalLimit is enough
+
+;--------------------------------------------------------------------
+; Evaluates whether the vertical limit has been reached.
+; Input: A  -> Vertical limit (TTLLLLSSS).
+;        HL -> Current position (010TTSSS LLLCCCCCCC).
+;
+; Alters the value of the AF and BC registers.
+;--------------------------------------------------------------------
+checkVerticalLimit:
+ld              b,      a                       ; Stores the value of A in B
+ld              a,      h                       ; A = value of H (010TTSSSSSS)
+and             $18                             ; Keeps the third
+rlca
+rlca
+rlca                                            ; Sets the value of the third in bits 6 and 7
+ld              c,      a                       ; Load the value in C
+ld              a,      h                       ; A = value of H (010TTSSSSSS)
+and             $07                             ; Keeps the scanline
+or              c                               ; Add the third
+ld              c,      a                       ; Load the value in C
+ld              a,      l                       ; A = value of L (LLLCCCCCCC)
+and             $e0                             ; Keeps the line
+rrca
+rrca                                            ; Puts the line on bits 3, 4 and 5
+or              c                               ; Adds third and scanline. A = TTLLLSSS
+cp              b                               ; Compare with B = original value A = boundary
+ret
